0e7af7b52a155fd8b027712d24a2c42b35a928ab
ABDELLAH MOUTAWAKKIL <abdo.moutawakkil2002@gmail.com>
2024-08-19 02:20:53 +0100

    first commit

---
+++ aji-n9raw-kitab-api/src/main/java/com/abde/aji_n9raw_kitab_api/AjiN9rawKitabApiApplication.java
@@ -0,0 +1,13 @@
+package com.abde.aji_n9raw_kitab_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjiN9rawKitabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjiN9rawKitabApiApplication.class, args);
+	}
+
+}
---
+++ aji-n9raw-kitab-api/src/test/java/com/abde/aji_n9raw_kitab_api/AjiN9rawKitabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abde.aji_n9raw_kitab_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjiN9rawKitabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
